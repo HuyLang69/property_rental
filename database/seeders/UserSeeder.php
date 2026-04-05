de97c4afaa9477b373153b6d576fa1b6ba499af5
HuyLang69 <langhuy87@gmail.com>
2026-04-06 01:58:08 +0700

    Add admin user to seeder + run seeder on deploy

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,19 +4,31 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
     public function run(): void
     {
-        // One known user so you can always log in during development
+        // Create admin user
+        User::create([
+            'first_name' => 'Admin',
+            'last_name'  => 'User',
+            'name'       => 'Admin User',
+            'email'      => 'admin@nestaway.com',
+            'password'   => Hash::make('AdminPass123!'),
+            'is_admin'   => true,
+            'email_verified_at' => now(),
+        ]);
+
+        // Regular test user
         User::create([
             'first_name' => 'Test',
             'last_name'  => 'User',
             'email'      => 'test@test.com',
             'password'   => bcrypt('password'),
             'bio'        => 'I am the test user.',
-            'is_admin'   => true,  // ← ADD THIS LINE!
+            'is_admin'   => false,
         ]);
 
         // 9 additional random users
